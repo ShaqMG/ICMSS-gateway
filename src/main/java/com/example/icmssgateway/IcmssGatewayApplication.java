24aa71ebf428a063b605dea5c1f2eb26f2b4fd80
ShaqMG <shalva.mgebrishvili.1@gmail.com>
2024-04-06 14:09:46 +0400

    Initial commit

---
+++ src/main/java/com/example/icmssgateway/IcmssGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.icmssgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcmssGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcmssGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/icmssgateway/IcmssGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icmssgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcmssGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
